f41065a5d7a45d4ce4e0f304ca88f3e71b87841a
Dice Gamer <46692071+ElDicee@users.noreply.github.com>
2022-12-01 18:27:59 +0100

    Changes on working

--- CardStudyApp.py
+++ CardStudyApp.py
@@ -4,21 +4,23 @@ from kivy.uix.widget import ObjectProperty
 from os import listdir
 from random import choice
 from kivy.clock import Clock
+from enum import Enum
 
 
 class DImage():
-
     def __init__(self, path):
         self.path = path
         self.extensionName = path.split("/")[-1] if "/" in path else path
-        self.name = self.extensionName[:-3] if "/" in path else self.extensionName[:-3]
+        self.name = self.extensionName[:-4] if "/" in path else self.extensionName[:-4]
+        self.tag = self.name.split("-")[1]
+        self.id = self.name.split("-")[0]
 
 
 class RCGame(BoxLayout):
     inptxt = ObjectProperty(None)
     ansLabel = ObjectProperty(None)
     displayImg = ObjectProperty(None)
-    img = DImage("images/img.png")
+    img = DImage("images/" + choice(listdir("images")))
 
     def nextImage(self, time):
         l = listdir("images")
@@ -31,21 +33,31 @@ class RCGame(BoxLayout):
         self.displayImg.source = self.img.path
 
     def checkAns(self):
-        if self.img.name == self.inptxt.text:
-            self.ansLabel.color = self.greenColor
-            self.ansLabel.text = "CORRECT"
+        if not str(self.inptxt.text).rstrip().lstrip() == "":
+            if str(self.img.tag).lower() == str(self.inptxt.text).lower():
+                self.ansLabel.text = "CORRECT"
+                print(Color.GREEN.value)
+                self.ansLabel.color = Color.GREEN.value
+                self.inptxt.text = ""
+            else:
+                self.ansLabel.text = "WRONG"
+                self.ansLabel.color = Color.RED.value
+                self.inptxt.text = ""
+            self.img.path = ""
+            Clock.schedule_once(callback=self.nextImage, timeout=1.5)
         else:
-            self.ansLabel.color = self.redColor
-            self.ansLabel.text = "WRONG"
-
-        self.img.path = ""
-        Clock.schedule_once(callback=self.nextImage, timeout=1.5)
+            self.ansLabel.text = "WAITING ANSWER..."
 
 
 class RCApp(App):
     def build(self):
         game = RCGame()
         return game
+
+
+class Color(Enum):
+    RED = [241, 36, 36, 1]
+    GREEN = [91, 221, 77, 1]
 
 
 if __name__ == "__main__":
